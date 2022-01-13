15c63cfd1d0249d9a4636dc9c7b0a798d627eede
Tahir Cesur <delenkar2825@gmail.com>
2022-01-13 20:33:23 +0300

    F01

--- utdbank-frontend/src/App.js
+++ utdbank-frontend/src/App.js
@@ -1,29 +1,23 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Footer from './components/common/Footer';
-import TopBar from './components/common/TopBar';
-import FaqsPage from './pages/FaqsPage';
-import HomePage from './pages/HomePage'
-import ServicesPage from './pages/ServicesPage';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/common/Footer";
+import TopBar from "./components/common/TopBar";
+import FaqsPage from "./pages/FaqsPage";
+import HomePage from "./pages/HomePage";
+import ServicesPage from "./pages/ServicesPage";
+import ScrollToTop from "./components/common/ScrollToTop.js";
 
 function App() {
   return (
-      <BrowserRouter>
-      <TopBar/>
+    <BrowserRouter>
+      <TopBar />
+      <ScrollToTop />
       <Routes>
-      <Route path="/faqs" element={ <FaqsPage/>}/>
-      <Route path="/services" element={ <ServicesPage/>}/>
-      <Route path="/" element={<HomePage/>}/>
+        <Route path="/faqs" element={<FaqsPage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
-      <Footer/>
-      </BrowserRouter>
-
-    
-
-
-   
-    
-    
+      <Footer />
+    </BrowserRouter>
   );
 }
 
